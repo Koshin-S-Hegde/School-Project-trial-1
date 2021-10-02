4abe50b7b06c9ab3f8c7ae88fe9c81ecec118315
LONEZAD <85434623+LONEZAD@users.noreply.github.com>
2021-10-02 15:01:31 +0530

    Closes #9; Changes all gettters and setters of objects from protected to public.

--- src/application/game/game_objects/obstacle.py
+++ src/application/game/game_objects/obstacle.py
@@ -7,5 +7,5 @@ from src.objects import Object
 class Obstacle(Object):
     def __init__(self, *groups: AbstractGroup) -> None:
         super().__init__(*groups)
-        self._set_size(Vector2(50, 5))
-        self._set_position(Vector2(50, 90))
+        self.set_size(Vector2(50, 5))
+        self.set_position(Vector2(50, 90))
