e921c88fb5fd71f628a8e4bb50edcae1b78bf36b
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-10-12 20:51:13 +0530

    Now you can change frame even if it did not originally exist.

--- src/objects/animated_object.py
+++ src/objects/animated_object.py
@@ -21,6 +21,9 @@ class AnimatedObject(Object):
         self.__frame_list.append(pygame.image.load(sprite_path))
 
     def change_frame(self, frame_number: int, sprite_path: str) -> None:
+        if frame_number > len(self.__frame_list) - 1:
+            self.add_animation_sprite("images/default.png")
+            return self.change_frame(frame_number, sprite_path)
         self.__frame_list[frame_number] = pygame.image.load(sprite_path)
 
     def set_custom_frame(self, frame_number: int) -> None:
