0bd66572fbf6833220610d7fc4146524cd014b92
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-10-26 21:25:15 +0530

    Made enemies compatible with level configurations.

--- src/application/game/game_objects/enemy/enemy.py
+++ src/application/game/game_objects/enemy/enemy.py
@@ -6,7 +6,15 @@ from src.objects.living_object import LivingObject
 
 
 class Enemy(LivingObject, Object):
-    def __init__(self, *groups: AbstractGroup) -> None:
-        super().__init__(10, *groups)
-        self.set_size(Vector2(10, 10))
-        self.set_position(Vector2(10, 10))
+    def __init__(
+            self,
+            health: float,
+            size: Vector2,
+            position: Vector2,
+            image_path: str,
+            *groups: AbstractGroup
+    ) -> None:
+        super().__init__(health, *groups)
+        self.set_size(size)
+        self.set_image_path(image_path)
+        self.set_position(position)
