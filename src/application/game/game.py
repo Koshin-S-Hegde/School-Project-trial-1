b10b7c0319302c12edd96ce2a5a86de8b277c63a
Koshin-S-Hegde <koshin-s-hegde@gmail.com>
2022-02-28 22:53:05 +0530

    Nuked all gameobjects

--- src/application/game/game.py
+++ src/application/game/game.py
@@ -1,70 +1,23 @@
 import pygame.event
-from pygame import Vector2
 
-from src.application.game.game_objects.platform.platform_handler import PlatformHandler, PlatformConfiguration
-from src.application.game.game_objects.enemy.enemy_handler import EnemyHandler, EnemyConfiguration
-from src.application.game.game_objects.player.player_handler import PlayerHandler, PlayerConfiguration
 from src.application_handling import application_events
 from src.application_handling.scenes.scene import Scene
 from src.event_handling import event_handler
 
 
 class Game(Scene):
-    __platform_handler: PlatformHandler
-    __enemy_handler: EnemyHandler
-    __player_handler: PlayerHandler
-
     def restart(self, event: pygame.event.Event = None) -> None:
         super().restart()
         self.__load_game_objects()
 
     def __load_game_objects(self) -> None:
-        self.__player_handler = PlayerHandler(
-            PlayerConfiguration(
-                jump_velocity=400,
-                gravity=2000,
-                position=Vector2(50, 50),
-                size=Vector2(5, 10),
-                health=1
-            )
-        )
-        self.__platform_handler = PlatformHandler(
-            PlatformConfiguration(
-                image_path="images/obstacle.png",
-                size=Vector2(40, 5),
-                position=Vector2(50, 90)
-            )
-        )
-        self.__enemy_handler = EnemyHandler(
-            EnemyConfiguration(
-                health=10,
-                position=Vector2(10, 10),
-                size=Vector2(10, 10),
-                image_path="images/default_object.png"
-            ))
+        pass
 
     def update(self, delta_time: float) -> None:
         super(Game, self).update(delta_time=delta_time)
-        self.__player_handler.update(
-            delta_time,
-            self.__platform_handler.platform_group,
-            self.__enemy_handler.bullet_group
-        )
-        self.__enemy_handler.update(delta_time, self.__player_handler.player, self.__player_handler.bullet_group)
-        self.__platform_handler.update(delta_time)
-        pygame.sprite.groupcollide(self.__platform_handler.platform_group, self.__player_handler.bullet_group, False,
-                                   True)
-        pygame.sprite.groupcollide(self.__platform_handler.platform_group, self.__enemy_handler.bullet_group, False,
-                                   True)
-        # pause menu
-        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
-            self.__start_pause_menu()
 
     def render(self) -> None:
         super(Game, self).render()
-        self.__enemy_handler.render()
-        self.__player_handler.render()
-        self.__platform_handler.render()
 
     @staticmethod
     def __start_pause_menu():
