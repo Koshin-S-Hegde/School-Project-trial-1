7705fed8bd6577a13125e515d0a8363081b690b2
LONEZAD <85434623+LONEZAD@users.noreply.github.com>
2022-03-07 22:30:36 +0530

    fixed a bug in delta time.

--- src/application_handling/scenes/scene_group.py
+++ src/application_handling/scenes/scene_group.py
@@ -6,14 +6,19 @@ from src.application_handling.scenes.scene import Scene
 class SceneGroup:
     __scenes: list[Scene]
     __last_time_update_was_called: float
+    __first_frame_done: bool
 
     def __init__(self) -> None:
         self.__scenes = list()
         self.__last_time_update_was_called = time.time()
+        self.__first_frame_done = False
 
     def update(self) -> None:
         """Updates all scenes in the group that are not paused"""
-        delta_time: float = time.time() - self.__last_time_update_was_called
+        if not self.__first_frame_done:
+            self.__last_time_update_was_called = time.time()
+            self.__first_frame_done = True
+        delta_time: float = (time.time() - self.__last_time_update_was_called) * 10
         for scene in self.__scenes:
             if not scene.is_paused:
                 scene.update(delta_time)
